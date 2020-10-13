f770e5be2589c3af945fa120a397f38320d28ceb
Emma <40247894+si-wang@users.noreply.github.com>
2020-10-12 21:21:53 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    char topLeftCorner = '┌';
+    char topRightCorner = '┐';
+    char horizontalEdge = '─';
+    char verticalEdge = '│';
+    char spade = '♠';
+    char heart = '♥';
+    char diamond = '♦';
+    char club = '♣';
+    char backPattern = '░';
+  }
+}
